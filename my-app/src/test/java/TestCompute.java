610ee363cc17f7e2b64cef31a8dc4f51d424845e
aylinbrtc <a.barutcu@etu.edu.tr>
2024-03-31 01:51:01 +0300

    TestCompute.java ve TestUtil.java güncellendi.

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -1,17 +1,46 @@
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCompute {
-  Compute c;
 
-  @Test
-  public void example() {
-    MessageQueue mq = mock(MessageQueue.class);
-    c = new Compute(mq);
-    assertTrue(true);
-  }
+    private Compute compute;
+    private MessageQueue mq;
+
+    @Before
+    public void setUp() {
+        mq = mock(MessageQueue.class); 
+        compute = new Compute(mq);
+    }
+
+    @Test
+    public void example() {
+      MessageQueue mq = mock(MessageQueue.class);
+      compute = new Compute(mq);
+      assertTrue(true);
+    }
+
+    @Test
+    public void testCountNumberOfOccurrences_MixedCase() {
+        when(mq.size()).thenReturn(3);
+        when(mq.contains("Aylin")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("AYLİN");
+        when(mq.getAt(1)).thenReturn("aylin");
+        when(mq.getAt(2)).thenReturn("Aylin");
+        
+        assertEquals(1, compute.countNumberOfOccurrences("Aylin"));
+    }
+
+    @Test
+    public void testCountNumberOfOccurrences_EmptyQueue() {
+        when(mq.size()).thenReturn(0);
+        assertEquals(-1, compute.countNumberOfOccurrences("aylin"));
+    }
+
+    
+
 }
--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,16 +1,35 @@
+import org.junit.*;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.junit.Before;
-import org.junit.Test;
-
 public class TestUtil {
-  Util c;
+    private Util util;
 
-  @Before
-  public void setUp() { c = new Util(); }
+    @Before
+    public void setUp() {
+        util = new Util();
+    }
 
-  @Test
-  public void example() { assertTrue(true); }
+    @Test
+    public void example() { assertTrue(true); }
+
+    @Test
+    public void testSingleArgument() {
+        assertFalse("Tek argüman false dönmelidir", util.compute(1));
+    }
+
+    @Test
+    public void testNotDivisibleSumWithOddArguments() {
+        assertFalse("Toplamın hiçbir argümana bölünmemesi false dönmelidir", util.compute(19, 7, 17));
+    }
+
+
+    @Test
+    public void testEvenNumberArguments() {
+        assertFalse("Çift sayıda argüman false dönmelidir", util.compute(1, 2, 3, 4));
+    }
+
+
 }
